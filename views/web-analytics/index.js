bc0ca24e01684c638624d2f89ba6dd28783513fd
Oliver Cheng <oliver@v1020-wn-9-97.campus-dynamic.uwaterloo.ca>
2017-03-06 17:19:17 -0500

    web analytics

---
+++ views/web-analytics/index.js
@@ -0,0 +1,16 @@
+'use strict';
+const router = require('koa-router')();
+const db = require('../../db/data-service');
+
+router.get('/sales-trend-forecasting', function*() {
+  // an example of getting a mock data item and logging it.
+  const params = {
+    database: 'Marketing',
+    table: 'sales'
+  };
+
+  const response = yield db.scan(params);
+  yield this.render('./sales-trend-forecasting/index', {params: params, response: response});
+});
+
+module.exports = router;
