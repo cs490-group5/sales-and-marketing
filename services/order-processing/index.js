2eeb2cab1d408099356c9012062449f93d1f2344
houbojun <houbojun@hotmail.com>
2017-03-29 16:59:01 -0400

    add integration with team 2 and SCM

--- services/order-processing/index.js
+++ services/order-processing/index.js
@@ -3,11 +3,16 @@ const db = require('../../db/data-service');
 const _ = require('lodash');
 const superagent = require('superagent-promise')(require('superagent'), Promise);
 
+function getUser(params) {
+  return superagent
+    .get(`138.197.159.113/api/customers/${params.user_id}`);
+}
+
 function recordPayment(params) {
   // contact AIS team to create a payment transaction
 
-  // this is Group 1
-  return superagent
+  // Team 1
+  const teamOneRequest = superagent
     .post('52.168.10.209:1337/transaction')
     .send({
       subledgerId: 6, // for sales revenue
@@ -17,23 +22,35 @@ function recordPayment(params) {
     })
     .set('Content-Type', 'application/json')
     .catch((err) => {
-      throw new Error('Failed to record payment with AIS: ' + err);
+      throw new Error('Failed to record payment with AIS team 1: ' + err);
     });
 
-  // group 2 is pending
+  // Team 2
+  const teamTwoRequest = superagent
+    .post('138.197.148.171:8000/sales')
+    .send(params)
+    .set('Content-Type', 'application/json')
+    .catch((err) => {
+      throw new Error('Failed to record payment with AIS team 2: ' + err);
+    });
+
+  return Promise.all([teamOneRequest, teamTwoRequest]);
 
   // return Promise.resolve('successfully recorded the payment');
 }
 
 function scheduleDelivery(params) {
   // contact SCM team to schedule a delivery
-  // console.log({
-  //   order_id: params.orderId,
-  //   shipping_address: params.ship_addr,
-  //   model: params.product_name,
-  //   quantity: params.quantity
-  // });
-  return Promise.resolve('successfully scheduled a delivery'); //@TODO:this needs to be a POST request to SCM team
+  return superagent
+    .post('https://cs490group8scm.herokuapp.com/orders?client=MCLHHQOZSXORFR532T004GKOJWZKG1OOMP1WZ4EQ3JMBW')
+    .send(params)
+    .set('Authorization', 'aa9e6548c60a89d893d4992fb6232e8714d50b8787f1ecc2cf1095dbccfb')
+    .set('Content-Type', 'application/json')
+    .catch((err) => {
+      throw new Error('Failed to schedule delivery with SCM team 8: ' + err);
+    });
+
+  // return Promise.resolve('successfully scheduled a delivery'); //@TODO:this needs to be a POST request to SCM team
   // return Promise.reject(new Error('SCM goes boom!'));
 }
 
@@ -68,13 +85,18 @@ function saveOrder(params) {
 }
 
 function newOrder(params) {
-  return recordPayment(params)
-    .then((orderItem) => {
-      params.payment_status = true;
-      params.orderId = orderItem.id;
-      return scheduleDelivery(params)
+  return getUser(params)
+    .then((res) => {
+      const user = res.body;
+      params.user_name = `${user.firstname} ${user.lastname}`;
+      return recordPayment(params)
         .then(() => {
-          return saveOrder(params);
+          params.payment_status = true;
+          return saveOrder(params)
+            .then((orderItem) => {
+              params.order_id = orderItem.id;
+              return scheduleDelivery(params);
+            });
         });
     });
 }
